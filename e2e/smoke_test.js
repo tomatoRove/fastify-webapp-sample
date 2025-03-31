1f3c834f7c59a936b874e5eefb100519a7bb9c5c
Apple <apple@appurunoMacBook-Air.local>
2025-03-31 23:22:23 +0900

    E2Eテストツールのインストール

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -5,11 +5,12 @@ Scenario("example.comにアクセスする", ({ I }) => {
   I.see("Example Domain");
 });
 
-Scenario('Webサイトを開きログインする', ({ I }) => {
-  I.amOnPage("/"); // BASE_URLからの相対パスに書き換える
-  I.click("ログインする");
-  I.fillField("ユーザー名", "user1");
-  I.fillField("パスワード", "super-strong-passphrase");
-  I.click("ログイン");
-  I.see("user1 さん");
-})
+
+//Scenario('Webサイトを開きログインする', ({ I }) => {
+//  I.amOnPage("/"); // BASE_URLからの相対パスに書き換える
+//  I.click("ログインする");
+//  I.fillField("ユーザー名", "user1");
+//  I.fillField("パスワード", "super-strong-passphrase");
+// I.click("ログイン");
+//  I.see("user1 さん");
+//})
