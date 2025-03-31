6c5fffcab7fdaa5c0c5a8315f34583b94b97aa22
Apple <apple@appurunoMacBook-Air.local>
2025-04-01 00:13:09 +0900

    テスト対象のURLを環境変数から渡すように変更

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -2,7 +2,7 @@ SuiteOf('スモークテスト');
 
 
 Scenario('Webサイトを開きログインする', ({ I }) => {
-  I.amOnPage("https://fastify-webapp-sample-production-a1b8.up.railway.app/items"); 
+  I.amOnPage("/"); 
   I.click("ログインする");
   I.fillField("ユーザー名", "user1");
   I.fillField("パスワード", "super-strong-passphrase");
